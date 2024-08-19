0a4a0d2d402068c7fcd2c82a1744e7d04ff02bcb
wlovem <lalakasuper2@gmail.com>
2024-08-19 17:31:33 +0700

    Добавил шаблон сайта

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+
+const LogoUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/2048px-LEGO_logo.svg.png"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className="header">
+            <img src={LogoUrl} alt=""/>
+        </header>
+        <nav className="nav">
+            <div>
+                <div>Profile</div>
+                <div>Messages</div>
+                <div>News</div>
+                <div>Music</div>
+            </div>
+        </nav>
+        <div className="content">
+            <div>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKX9t1fDP4bYcE_skCUUzIllxgsvPvtDxmTg&s"
+                     alt="" className="headerAcc"/>
+            </div>
+            <div>
+                ava + desc
+            </div>
+            <div>
+                my posts
+                <div>
+                    new post
+                </div>
+                <div>
+                    all posts
+                    <div>
+                        post1
+                    </div>
+                    <div>
+                        post2
+                    </div>
+                </div>
+            </div>
+        </div>
+        <footer>
+            footer
+        </footer>
     </div>
   );
 }
 
-export default App;
+export default App
